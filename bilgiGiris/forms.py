c6c7d2c60500db0c99e594279cf68456140242fd
Refik Turkeli <refik.rfk@gmail.com>
2011-06-24 23:24:49 +0300

    form submission working

--- bilgiGiris/forms.py
+++ bilgiGiris/forms.py
@@ -1,22 +1,26 @@
 from django.forms import ModelForm
 from audio.bilgiGiris.models import Bilgi
 
-class AkademiForm(ModelForm):
-    class Meta:
-        model = Bilgi
-        exclude = ('mesaj',)
-
 class TeklifForm(ModelForm):
+    TEMPLATE = 'bilgiGiris/teklif.html'
     class Meta:
         model = Bilgi
         exclude = ('firma',)
 
+class AkademiForm(ModelForm):
+    TEMPLATE = 'bilgiGiris/akademi.bulten.iletisim.html'
+    class Meta:
+        model = Bilgi
+        exclude = ('mesaj',)
+
 class BultenForm(ModelForm):
+    TEMPLATE = 'bilgiGiris/akademi.bulten.iletisim.html'
     class Meta:
         model = Bilgi
         exclude = ('firma','mesaj',)
 
 class IletisimForm(ModelForm):
+    TEMPLATE = 'bilgiGiris/akademi.bulten.iletisim.html'
     class Meta:
         model = Bilgi
         exclude = ('sehir','firma',)
--- bilgiGiris/views.py
+++ bilgiGiris/views.py
@@ -3,13 +3,22 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import Http404
 
-def formGoster(request,tip):
+def formSec(tip):
     if tip == 'teklif':
-        template = 'bilgiGiris/teklif.html'
-    elif tip == 'bulten' or tip == 'akademi' or tip == 'iletisim':
-        template = 'bilgiGiris/akademi.bulten.iletisim.html'
+        return TeklifForm
+    elif tip == 'bulten':
+        return BultenForm
+    elif tip == 'akademi':
+        return  AkademiForm
+    elif tip == 'iletisim':
+        return IletisimForm
     else:
         raise Http404
-    form = TeklifForm()
-    return render_to_response(template,{'form':form},context_instance=RequestContext(request))
-    
+
+def formIslem(request,tip):
+    form = formSec(tip)
+    if request.method == 'POST':
+        bilgi = form(request.POST)
+        bilgi.save()
+    return render_to_response(form().TEMPLATE,{'form':form()},context_instance=RequestContext(request))
+
