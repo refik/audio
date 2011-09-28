f9607df6c44c922efbf1a4edd03ee0209308fcc6
Refik Turkeli <refik.rfk@gmail.com>
2011-09-28 10:49:19 +0300

    grid issue solved

--- calisanProfil/views.py
+++ calisanProfil/views.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from django.contrib.auth.models import User
 from django.shortcuts import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -8,7 +9,7 @@ def temsilci(request):
     try:
         sorumlu = User.objects.filter(profile__sorumluSehir__isim__contains = request.GET['sehir'])
         temsilci = sorumlu[0]
-        kisi = {'Isim' : temsilci.first_name + ' ' + temsilci.last_name, 'Telefon' : temsilci.profile.telefon, 'Email' : temsilci.email}
+        kisi = {'İsim' : temsilci.first_name + ' ' + temsilci.last_name, 'Telefon' : temsilci.profile.telefon, 'Email' : temsilci.email}
     except:
         kisi ={}
     return HttpResponse(json.dumps(kisi))    
