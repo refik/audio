c48e9062985069b8e44029ee0a258f9a342df420
Refik Turkeli <refik.rfk@gmail.com>
2011-09-28 15:45:00 +0300

    mailing by ntlm

---
+++ bilgiGiris/mail.py
@@ -0,0 +1,13 @@
+from ntlm.smtp import ntlm_authenticate
+import smtplib
+
+def audiomail(kimden,kime,konu,mesaj):
+    server = smtplib.SMTP("mail.audio.com.tr")
+    server.ehlo()
+    ntlm_authenticate(server, r"audio\audioweb", "4455")
+    fromaddr = kimden
+    toaddrs  = kime
+    msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (fromaddr, ", ".join(toaddrs),konu))
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+
