7e1c219cc5ba32942430b772fcb4bec360716e34
Refik Turkeli <refik.rfk@gmail.com>
2011-09-28 15:46:31 +0300

    mailing by ntlm

--- bilgiGiris/views.py
+++ bilgiGiris/views.py
@@ -47,7 +47,7 @@ def formIslem(request,tip):
                 bilgi_db.sorumlu.add(sorumlu)
             konu = 'Audio ' + tip + ' Formu'
             mesaj = mesajOlustur(bilgi.cleaned_data)
-            audiomail('audioweb@audio.com',['refik.rfk@gmail.com'],konu,mesaj)
+            audiomail('audioweb@audio.com.tr',['refik.rfk@gmail.com'],konu,mesaj)
             yollaForm = form()
             geri_donus = 'Isteginiz Elimize Ulasmistir'
         else:
