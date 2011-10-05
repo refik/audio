d6b95b624f9dfd420c049301dbde2136431d80a2
Refik Türkeli <refik.rfk@gmail.com>
2011-10-05 19:28:45 +0000

    security

--- bilgiGiris/mail.py
+++ bilgiGiris/mail.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 from ntlm.smtp import ntlm_authenticate
+from audio.sifre import DOMAIN, PASSWORD
 import smtplib
 
 def audiomail(kimden,kime,konu,mesaj):
     server = smtplib.SMTP("mail.audio.com.tr")
     server.ehlo()
-    ntlm_authenticate(server, r"audio\audioweb", "4455")
+    ntlm_authenticate(server, DOMAIN, PASSWORD)
     fromaddr = kimden
     toaddrs  = kime
     message = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n%s" % (fromaddr, ", ".join(toaddrs),konu,mesaj))
