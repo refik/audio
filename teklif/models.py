fd62f0168a3ad057a56e76ee70781e59addd884e
Refik Turkeli <refik.rfk@gmail.com>
2011-11-02 18:27:47 +0200

    comment moderation and preview improved

--- teklif/models.py
+++ teklif/models.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save, post_save
 from audio.bilgiGiris.models import Bilgi
 from django.contrib.comments.models import Comment
 from audio.bilgiGiris.mail import audiomail
@@ -12,11 +12,12 @@ def yorum_yonet(sender,**kwargs):
         yorum.content_object.teklif
         if yorum.durum != None: 
             yorum.content_object.teklif.durum = yorum.durum
-            yorum.content_object.teklif.save()
             durum_mesaji = u"(Bu işin durumu '%s' olarak değiştirildi)" % yorum.durum
+            yorum.comment += ' %s' % durum_mesaji
+            yorum.content_object.teklif.save()
     except:
         pass
-    audiomail("audioweb@audio.com.tr",[sorumlu.email for sorumlu in yorum.content_object.sorumlu.all()] + ['refik.rfk@gmail.com'],str(yorum.content_object.tip) + u'\' na Yorum Yapıldı',u'%d nolu müşteri isteğine yapılan yorum\n\n%s %s\n\nhttp://www.audio.com.tr/takip/%d adresinden detaylı inceleyebilirsiniz'%(yorum.content_object.pk,yorum.comment,durum_mesaji,yorum.content_object.pk))
+    #audiomail("audioweb@audio.com.tr",[sorumlu.email for sorumlu in yorum.content_object.sorumlu.all()] + ['refik.rfk@gmail.com'],str(yorum.content_object.tip) + u'\' na Yorum Yapıldı',u'%d nolu müşteri isteğine yapılan yorum\n\n%s %s\n\nhttp://www.audio.com.tr/takip/%d adresinden detaylı inceleyebilirsiniz'%(yorum.content_object.pk,yorum.comment,durum_mesaji,yorum.content_object.pk))
 
 def teklif_yarat(sender,**kwargs):
     try:
@@ -42,6 +43,6 @@ class TeklifYorum(Comment):
     durum = models.ForeignKey(Durum,null=True,blank=True)
 
 post_save.connect(teklif_yarat,sender=Bilgi)
-post_save.connect(yorum_yonet,sender=TeklifYorum)
+pre_save.connect(yorum_yonet,sender=TeklifYorum)
 
 
