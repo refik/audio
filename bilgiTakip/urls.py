14ed8b08dab620e8a7e8795d83d06ff935badfea
Refik Türkeli <refik.rfk@gmail.com>
2011-12-21 15:15:08 +0000

    template issues

--- bilgiTakip/urls.py
+++ bilgiTakip/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls.defaults import *
 from audio.bilgiTakip.views import BilgiDetailView, TipListView, IstatistikView, TemsilciDetailView, TemsilciListView
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 
 urlpatterns = patterns('',
-    (r'^$', login_required(TipListView.as_view())),
+    (r'^$', never_cache(login_required(TipListView.as_view()))),
     (r'^(?P<pk>\d+)/$', login_required(BilgiDetailView.as_view())),
     (r'^istatistikler/$', login_required(IstatistikView.as_view())),
     (r'^temsilciler/$', login_required(TemsilciListView.as_view())),
