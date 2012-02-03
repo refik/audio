ace55b846504d833f986d9b5ae035ea37dabeb77
Refik Türkeli <refik.rfk@gmail.com>
2012-02-03 14:50:27 +0000

    FORGOT THE FILE

---
+++ ortakVeri/sitemap.py
@@ -0,0 +1,12 @@
+from django.contrib.sitemaps import Sitemap
+
+class Site(object):
+    def __init__(self,url):
+        self.url = url
+
+    def get_absolute_url(self):
+        return self.url
+
+class EkstraSitemap(Sitemap):
+    def items(self):
+        return [Site('/teklif-formu'), Site('/iletisim-formu'), Site('/akademi-formu')]
