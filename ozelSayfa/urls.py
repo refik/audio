536fd1d51a70a503f5f74b97ce989913e661e163
Refik Türkeli <refik.rfk@gmail.com>
2012-02-20 12:46:12 +0000

    production code

--- ozelSayfa/urls.py
+++ ozelSayfa/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.defaults import *
+from django.contrib.auth.views import logout, password_change, password_change_done
 
 urlpatterns = patterns('audio.ozelSayfa.views',
     (r'^$', 'anasayfa'),
     (r'^logout/','logout_view'),
+    (r'^sifre-degistir/', password_change),
+    (r'^password/done/$', password_change_done),
 #    (r'^kalite-belgelerimiz/','kaliteBelgelerimiz'),
 )
