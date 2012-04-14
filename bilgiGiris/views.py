62838452f470a9824db29409d7736c064da2a6c7
Refik Turkeli <refik.rfk@gmail.com>
2012-04-14 22:15:00 +0300

    automated offer system bindings

--- bilgiGiris/views.py
+++ bilgiGiris/views.py
@@ -1,12 +1,15 @@
 # coding: utf-8
 from audio.bilgiGiris.forms import TeklifForm, BultenForm, AkademiForm, IletisimForm
 from audio.bilgiGiris.models import Tip
+from audio.teklif.models import OtomatikTeklif
 from audio.ortakVeri.mail import audiomail
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import Http404
 from django.db.models import Q
+import json
 SIRA = ['isim','email','sehir','firma','telefon','mesaj']
 
 def mesajOlustur(sozluk):
@@ -30,15 +33,54 @@ def formSec(tip):
     else:
         raise Http404
 
+def state_to_message(state, message):
+    translations = {
+        'single': 'apartman', 'multiple': 'site', 'villa': 'villa', 'two-doors': 'iki kapi', 'extra-monitor': 'ekstra sube', 
+        'security': 'guvenlik', 'extra-camera': 'ekstra kamera', 'market': 'marketle konusma', 'doormen': 'kapici', 
+        'light-base': 'isikli panel altligi', '': ''
+    }
+    t = lambda s: translations[s]
+    info_tuple = (message, t(state['building']), state['apartment'], state['block'], 
+        state['monitor']['id'], state['panel']['id'], 
+        ', '.join([t(extra) for extra in state['extra']['monitors']] + [t(extra) for extra in state['extra']['panels']]), 
+        state['price'])
+    formatted_message = \
+"""%s ****Otomatik teklif sisteminiden iletilmistir. Musterinin sistemden sectileri****
+[Bina: %s | Toplam Daire: %i | Blok: %i] - 
+[Monitor: %s | Panel: %s] - 
+[Ekstra: %s] - 
+[Fiyat: %iTL]""" % info_tuple
+    return formatted_message
+
+@csrf_exempt
 def formIslem(request,tip):
     form = formSec(tip)
+    record = None # Will be used if request is from auto offer
+    sub_dict = lambda d, keys: {k:v for k,v in d.iteritems() if k in keys}
     if request.method == 'POST':
-        bilgi = form(request.POST)
+        post_dict = request.POST.copy() # Copy so it can be processed
+
+        if post_dict.get('type','') == 'offer': # Check if it came from offer site
+            message = post_dict.get('mesaj') # Original message
+            state = post_dict.get('state') # State object to better inform
+            print json.loads(state)
+            request_info = state_to_message(json.loads(state), message) # Addition to original message
+            post_dict.setlist('mesaj', [request_info]) # Update with new message
+            sub_meta = sub_dict(request.META, ['REMOTE_ADDR', 'HTTP_USER_AGENT', 'HTTP_REFERER']) # Get customer info
+            record = OtomatikTeklif(musteri=json.dumps(sub_meta), durum=state) # Pack usefull info
+
+        bilgi = form(post_dict)
         if bilgi.is_valid():
             bilgi_db = bilgi.save()
             tip_db = Tip.objects.get(isim__contains = tip)
             bilgi_db.tip = tip_db 
             bilgi_db.save()
+
+            # Save the record for auto offer
+            if record: 
+                record.teklif = bilgi_db.teklif
+                record.save()
+
             sorumlular = User.objects.filter(profile__sorumluTip__isim__contains = tip)
             if tip == 'teklif':
                 sorumlular = sorumlular.filter(profile__sorumluSehir__isim__contains = bilgi.cleaned_data['sehir']).exclude(Q(profile__birincil=True) | Q(profile__ikincil=True))
