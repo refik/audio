a0a2ee3451eb4cdf535a33940516a5d625c25efb
Refik Türkeli <refik.rfk@gmail.com>
2013-04-25 03:55:42 +0000

    get people random

--- calisanProfil/views.py
+++ calisanProfil/views.py
@@ -5,10 +5,14 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 from django.contrib.auth import authenticate, login
 import json
+import random
 
 def temsilci(request):
     try:
-        temsilci = User.objects.get(profile__ikincil=True,profile__sorumluSehir__isim__contains = request.GET['sehir'])
+        temsilci = User.objects.filter(profile__ikincil=True,profile__sorumluSehir__isim__contains = request.GET['sehir'])
+        if not temsilci:
+            temsilci = User.objects.filter(profile__birincil=True,profile__sorumluSehir__isim__contains = request.GET['sehir'])
+        temsilci = random.choice(temsilci)
         kisi = {'İsim' : temsilci.get_full_name(), 'Telefon' : temsilci.profile.telefon, 'Email' : temsilci.email}
     except:
         kisi ={}
